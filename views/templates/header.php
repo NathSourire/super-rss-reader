bd977590482598a7509d76e98e955fcc65588f17
NathSourire <nathsourire@hotmail.com>
2023-09-11 14:58:04 +0200

    first commit

---
+++ views/templates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=*, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script defer src="/public/assets/js/script.js"></script>
+    <link rel="stylesheet" href="/public/assets/css/style.css">
+    <title>Super RSS Reader</title>
+</head>
+<body>
+    
