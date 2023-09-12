ac8ccc1962aa47ebf05b25e959d37ce97ca1e8b7
Pierre <demaret.pierre.pro@gmail.com>
2023-09-12 16:43:48 +0200

    home-pages presque finie

---
+++ views/home.php
@@ -0,0 +1,83 @@
+<main>
+    <!-- sujet 1 -->
+    <div class="line"></div>
+    <div class="container-fluid">
+        <h4 class="text-center my-3">Sujet 1 ( futur variable )</h4>
+        <div class="line mb-3"></div>
+        <?php
+        $flux = simplexml_load_file('https://www.jeuxactu.com/rss/ja.rss');
+        if ($flux) {
+            $titles = array();
+            foreach ($flux->channel->item as $item) {
+                $title = $item->title;
+                $titles[] = $title;
+            }
+
+
+            for ($i = 0; $i < 6; $i++) {
+                $currentTitle = isset($titles[$i]) ? $titles[$i] : '';
+        ?>
+                <div class="border d-flex justify-content-between p-2 my-2 accueil-actu">
+                    <h5 class="m-0 pt-2 title-text"><?= $currentTitle ?></h5>
+                    <img src="/public/assets/img/arrow-right.png" alt="arrow-right">
+                </div>
+        <?php }
+        }
+        ?>
+    </div>
+    <div class="line"></div>
+    <!--  -->
+    <!-- sujet 2 -->
+    <div class="container-fluid">
+        <h4 class="text-center my-3">Sujet 2 ( futur variable )</h4>
+        <div class="line mb-3"></div>
+        <?php
+        $flux = simplexml_load_file('https://www.jeuxactu.com/rss/ja.rss');
+        if ($flux) {
+            $titles = array();
+            foreach ($flux->channel->item as $item) {
+                $title = $item->title;
+                $titles[] = $title;
+            }
+
+
+            for ($i = 0; $i < 6; $i++) {
+                $currentTitle = isset($titles[$i]) ? $titles[$i] : '';
+        ?>
+                <div class="border d-flex justify-content-between p-2 my-2 accueil-actu">
+                    <h5 class="m-0 pt-2 title-text"><?= $currentTitle ?></h5>
+                    <img src="/public/assets/img/arrow-right.png" alt="arrow-right">
+                </div>
+        <?php }
+        }
+        ?>
+    </div>
+    <div class="line"></div>
+    <!--  -->
+    <!-- sujet 3 -->
+    <div class="container-fluid">
+        <h4 class="text-center my-3">Sujet 3 ( futur variable )</h4>
+        <div class="line mb-3"></div>
+        <?php
+        $flux = simplexml_load_file('https://www.jeuxactu.com/rss/ja.rss');
+        if ($flux) {
+            $titles = array();
+            foreach ($flux->channel->item as $item) {
+                $title = $item->title;
+                $titles[] = $title;
+            }
+
+
+            for ($i = 0; $i < 6; $i++) {
+                $currentTitle = isset($titles[$i]) ? $titles[$i] : '';
+        ?>
+                <div class="border d-flex justify-content-between p-2 my-2 accueil-actu">
+                    <h5 class="m-0 pt-2 title-text"><?= $currentTitle ?></h5>
+                    <img src="/public/assets/img/arrow-right.png" alt="arrow-right">
+                </div>
+        <?php }
+        }
+        ?>
+    </div>
+    <!--  -->
+</main>
