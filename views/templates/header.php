fdd2ab30fe99513d2e089b67c7ee9f23ce913b82
NathSourire <nathsourire@hotmail.com>
2023-09-12 10:02:13 +0200

    Logo

--- views/templates/header.php
+++ views/templates/header.php
@@ -15,7 +15,7 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
+                <img src="/public/assets/img/Gaming.png" alt="Logo Gaming" width="90" height="90">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -23,32 +23,22 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="/controllers/home-controller.php">Accueil</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="/controllers/user-choice-controller.php">Faites votre choix</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
+                            Vos sujet
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li><a class="dropdown-item" href="#">Sujet 1</a></li>
+                            <li><a class="dropdown-item" href="#">Sujet 2</a></li>
+                            <li><a class="dropdown-item" href="#">Sujet 3</a></li>
                         </ul>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
                 </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
             </div>
         </div>
     </nav>
---
+++ views/user-choice.php
@@ -0,0 +1,16 @@
+<form id="formchoice" enctype="multipart/form-data" method="get">
+    <fieldset class="container-fluid">
+        <div class="row">
+            <label for=""></label>
+            <input type="checkbox" name="" id="">
+            <label for=""></label>
+            <input type="checkbox" name="" id="">
+            <label for=""></label>
+            <input type="checkbox" name="" id="">
+            <label for=""></label>
+            <input type="checkbox" name="" id="">
+            <label for=""></label>
+            <input type="checkbox" name="" id="">
+        </div>
+    </fieldset>
+</form>
