fdc9e7d52e023e75d45f19195cbe49baf7e5c97c
NathSourire <nathsourire@hotmail.com>
2023-09-12 12:10:41 +0200

    checkbox end

---
+++ config/constant.php
@@ -0,0 +1,2 @@
+<?php 
+define('ACTUALITY', ['Toutes les Astuces et Soluces','Toute l\'actualité Arcade ','Toute l\'actualité Mobil','Toute l\'actulité PC ',' Les prochaines sorties']);
--- views/user-choice.php
+++ views/user-choice.php
@@ -1,16 +1,15 @@
+<h3 class="text-center">Choisir 3 sujet d'actu</h3>
 <form id="formchoice" enctype="multipart/form-data" method="get">
     <fieldset class="container-fluid">
-        <div class="row">
-            <label for=""></label>
-            <input type="checkbox" name="" id="">
-            <label for=""></label>
-            <input type="checkbox" name="" id="">
-            <label for=""></label>
-            <input type="checkbox" name="" id="">
-            <label for=""></label>
-            <input type="checkbox" name="" id="">
-            <label for=""></label>
-            <input type="checkbox" name="" id="">
+        <div>
+            <label class="form-label"></label><br>
+            <?php
+            foreach (ACTUALITY as $key => $value) { ?>
+                <label for="actuality_<?= $key ?>"></label>
+                <input class="my-3 ms-5" id="actuality_<?= $key ?>" type="checkbox" name="actuality[]" value="<?= $key ?>">
+                <?= $value ?><br>
+            <?php } ?>
         </div>
+            <button type="button" class="btn ms-5 my-3" id="btnsubmit">Envoie !</button>
     </fieldset>
 </form>
