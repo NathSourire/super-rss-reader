4af7c442618a7abea0d32d573f5220b2a85edc5d
Allan Baguet <allan.baguet80@gmail.com>
2023-09-12 12:13:40 +0200

    article v1

---
+++ views/article.php
@@ -0,0 +1,29 @@
+<div class="container-fluid py-3">
+    <a href="/controllers/pages-controller.php">
+        <button type="button" class="btn" id="btnBefore">Retour aux articles</button>
+    </a>
+    <div class="row">
+        <div class="col py-3">
+            <h3 class="text-center py-3 rounded" id="newsTitle">Actualités</h3>
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title text-center py-3">Titre de l'actualité</h4>
+                    <p class="cardDate">12/09/2023</p>
+                    <img src="/public/assets/img/Gaming.png" class="img-fluid py-2" alt="logo jeu vidéo">
+                    <p class="card-text text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam quidem, unde numquam 
+                    voluptatibus reprehenderit asperiores facere doloremque repellat, mollitia perferendis, ex ut ullam tenetur culpa!
+                    Dicta consequatur accusantium velit pariatur.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam quidem, unde numquam 
+                    voluptatibus reprehenderit asperiores facere doloremque repellat, mollitia perferendis, ex ut ullam tenetur culpa!
+                    Dicta consequatur accusantium velit pariatur.</p>
+                    <div class="d-flex flex-row-reverse">
+                        <a href="#">
+                            <img src="/public/assets/img/arrow-right-solid 1.png" alt="flèche vers la droite">
+                        </a>
+                        <p class="cardDate mx-3">Redirection vers le site officiel</p>
+                    </div>
+                </div>               
+            </div>
+        </div>
+    </div>
+</div>
--- views/user-choice.php
+++ views/user-choice.php
@@ -1,16 +1,27 @@
+<h3 class="text-center">Choisir 3 sujet d'actu</h3>
 <form id="formchoice" enctype="multipart/form-data" method="get">
     <fieldset class="container-fluid">
-        <div class="row">
-            <label for=""></label>
-            <input type="checkbox" name="" id="">
-            <label for=""></label>
-            <input type="checkbox" name="" id="">
-            <label for=""></label>
-            <input type="checkbox" name="" id="">
-            <label for=""></label>
-            <input type="checkbox" name="" id="">
-            <label for=""></label>
-            <input type="checkbox" name="" id="">
+        <div class="row  my-5">
+            <div>
+                <label for="choiceastuce"></label>
+                Toutes les Astuces et Soluces <input class="ms-5" type="checkbox" name="choiceAstuce" id="choiceastuce">
+            </div>
+            <div>
+                <label for="choicearcade"></label>
+                Toute l'actualité Arcade <input class="ms-5" type="checkbox" name="choiceArcade" id="choicearcade">
+            </div>
+            <div>
+                <label for="choicemobile"></label>
+                Toute l'actualité Mobile <input class="ms-5" type="checkbox" name="choiceMobile" id="choicemobile">
+            </div>
+            <div>
+            <label for="choicepc"></label>
+            Toute l'actulité PC <input class="ms-5" type="checkbox" name="choicePc" id="choicepc">
+            </div>
+            <div>
+            <label for="choicesortie"></label>
+            Les prochaines sorties <input class="ms-5" type="checkbox" name="choiceSortie" id="choicesortie">
+            </div>
         </div>
     </fieldset>
 </form>
