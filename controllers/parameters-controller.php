62f084fb7d4a4e8b9743fa2f1e73bd1d91883999
NathSourire <134375430+NathSourire@users.noreply.github.com>
2023-09-14 10:14:42 +0200

    four days

--- controllers/parameters-controller.php
+++ controllers/parameters-controller.php
@@ -19,5 +19,5 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
 
 
 include __DIR__ . '/../views/templates/header.php';
-include __DIR__ . '/../views/user-choice.php';
+include __DIR__ . '/../views/parameters.php';
 include __DIR__ . '/../views/templates/footer.php';
